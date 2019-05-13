96772c736f9219950f074903bed7a77620c19313
whatwewant <tobewhatwewant@gmail.com>
2019-05-13 14:18:46 +0800

    :white_check_mark:(core): add test for expiration mechanism

---
+++ test/expiration.spec.ts
@@ -0,0 +1,34 @@
+import { delay } from '@zcorky/delay';
+import storage from '../src';
+
+describe('expiration', () => {
+  it('1ms', async () => {
+    storage.set('expiration', 'value', 1);
+    expect(storage.get('expiration')).toEqual('value');
+
+    await delay(1);
+    expect(storage.has('expiration')).toBeTruthy()
+    expect(storage.get('expiration')).toEqual(null);
+    expect(storage.has('expiration')).toBeFalsy();
+  });
+
+  it('10ms', async () => {
+    storage.set('expiration', 'value', 10);
+    expect(storage.get('expiration')).toEqual('value');
+
+    await delay(10);
+    expect(storage.has('expiration')).toBeTruthy()
+    expect(storage.get('expiration')).toEqual(null);
+    expect(storage.has('expiration')).toBeFalsy();
+  });
+
+  it('100ms', async () => {
+    storage.set('expiration', 'value', 100);
+    expect(storage.get('expiration')).toEqual('value');
+
+    await delay(100);
+    expect(storage.has('expiration')).toBeTruthy()
+    expect(storage.get('expiration')).toEqual(null);
+    expect(storage.has('expiration')).toBeFalsy();
+  });
+});
